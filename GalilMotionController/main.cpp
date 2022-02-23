db11ce91f400a78731a9400440521f2b9366e673
Dimitri Lezcano <dalezcano19@gmail.com>
2022-02-23 16:00:04 -0500

    Initial Commit

---
+++ GalilMotionController/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GalilMotionController
+//
+//  Created by Dimitri Lezcano on 2/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
